c3f0885dcdcdfa9e08ac74a7f7965380a5b046d0
kaio-baleeiro <kaio.jesus@bandtec.com.br>
2022-05-24 02:42:49 -0300

    feat(Main.tf): adicionando backend s3

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+
+    backend "s3" {
+        bucket = "kaio-baleeiro-tfstate"
+        key = "terraform.tfstate"
+        region = "us-east-1"
+    }
+
     required_providers {
         aws = {
             source = "hashicorp/aws"
