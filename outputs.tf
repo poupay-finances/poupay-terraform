a35fbd482543f515c1cf5ea4ff4138aa7712da75
kaio-baleeiro <kaio.jesus@bandtec.com.br>
2022-05-24 02:19:32 -0300

    refactor(arquivos-antigos): deletando arquivos inutilizáveis

--- outputs.tf
+++
@@ -1,8 +0,0 @@
-output "server_ip_project" {
-    value = aws_instance.server_project.public_ip
-    description = "Ip público do web server"
-
-    depends_on = [
-        aws_eip.server_eip_project
-    ]
-}
