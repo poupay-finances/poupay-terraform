b132522e78de74716002d74ee2aa5a4892057721
kaio-baleeiro <61800714+kaio-baleeiro@users.noreply.github.com>
2022-05-26 20:55:37 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+    
+    backend "s3" {
+        bucket = "kaio-baleeiro-tfstate"
+        key = "kaio-baleeiro.tfstate"
+        region = "us-east-1"
+    }
+    
     required_providers {
         azurerm = {
           source  = "hashicorp/azurerm"
