55d38ef65a4f11213763b8d14582e85ca265d279
Kaio Silva Baleeiro de Jesus <61800714+kaio-baleeiro@users.noreply.github.com>
2022-08-26 21:20:00 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
 terraform {
-
-  backend "s3" {
-    bucket = "kaiobaleeiro"
-    key    = "kaio-baleeiro-terraform.tfstate"
-    region = "us-east-1"
-  }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
